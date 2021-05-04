85281df78e42f4f2179b0b8ba461f47e2483efe8
Requi <privat@requi-dev.de>
2021-05-04 05:59:09 +0200

    Version Bump.

--- FBT Saver/FBTSaver.cs
+++ FBT Saver/FBTSaver.cs
@@ -17,7 +17,7 @@ namespace FBT_Saver
         public const string Name = "FBT Saver";
         public const string Author = "Requi";
         public const string Company = "RequiDev";
-        public const string Version = "1.1.5";
+        public const string Version = "1.1.6";
         public const string DownloadLink = "https://github.com/RequiDev/FBTSaver";
     }
 
